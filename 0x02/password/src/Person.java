efe6aae1710df1a5bb32e11101b3c253f0dafef0
Joao Bohana <jbohana@ciandt.com>
2022-07-20 07:47:08 -0300

    tried to fix

--- 0x02/password/src/Person.java
+++ 0x02/password/src/Person.java
@@ -1,11 +1,11 @@
 package src;
 
 public class Person {
-    public boolean checkUser(String user){
+    public static boolean checkUser(String user){
         return user.matches("^[a-zA-Z0-9]{8,}$");
     }
 
-    public boolean checkPassword(String password){
+    public static boolean checkPassword(String password){
         return password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
     }
 }
