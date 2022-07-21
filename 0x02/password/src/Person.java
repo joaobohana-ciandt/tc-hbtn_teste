8cbe566ecc191a647653319a98947d93c96afaef
Joao Bohana <jbohana@ciandt.com>
2022-07-21 11:39:31 -0300

    made it better lol

--- 0x02/password/src/Person.java
+++ 0x02/password/src/Person.java
@@ -1,9 +1,20 @@
 public class Person {
-    public static boolean checkUser(String user){
-        return user.matches("^[a-zA-Z0-9]{8,}$");
+    private String username;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public static boolean checkPassword(String password){
-        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean checkUser(){
+        return this.username.matches("^[a-zA-Z0-9]{8,}$");
+    }
+
+    public boolean checkPassword(){
+        return this.password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
     }
 }
--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -5,47 +5,53 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PersonTest {
+    static Person person;
 
     @BeforeAll
     public static void setup(){
-
+        person = new Person();
     }
 
     @ParameterizedTest(name = "Check if user is valid")
     @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
     public void check_user_valid(String user){
-        assertTrue(Person.checkUser(user));
+        person.setUsername(user);
+        assertTrue(person.checkUser());
     }
 
     @ParameterizedTest(name = "Check if user is not valid")
     @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
     public void check_user_not_valid(String user){
-        assertFalse(Person.checkUser(user));
+        person.setUsername(user);
+        assertFalse(person.checkUser());
     }
 
     @ParameterizedTest(name = "Password without letters")
     @ValueSource(strings = {"123456789", "#$%1234"})
     public void does_not_have_letters(String password){
-        assertFalse(Person.checkPassword(password));
+        person.setPassword(password);
+        assertFalse(person.checkPassword());
     }
 
     @ParameterizedTest(name = "Password without numbers")
     @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
     public void does_not_have_numbers(String password){
-        assertFalse(Person.checkPassword(password));
+        person.setPassword(password);
+        assertFalse(person.checkPassword());
     }
 
     @ParameterizedTest(name = "Password with less than 8 characters")
     @ValueSource(strings = {"Abc@123", "12$@hbt"})
     public void does_not_have_eight_chars(String password){
-        assertFalse(Person.checkPassword(password));
+        person.setPassword(password);
+        assertFalse(person.checkPassword());
     }
 
     @ParameterizedTest(name = "Password is valid")
     @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
     public void check_password_valid(String password){
-        assertTrue(Person.checkPassword(password));
+        person.setPassword(password);
+        assertTrue(person.checkPassword());
     }
-
 
 }
